749140de6c39db2927e0ac2821ed206882128dcc
Elton Arruda <eltonbugsantos.format@gmail.com>
2024-01-05 22:42:08 -0300

    Desafio de Código do bootcamp Decola Tech 2024 - na plataforma DIO.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smatphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "1122334455", modelo: "Modelo 1", imei: "6677889900", memoria: 123);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smarthphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "5544332211", modelo: "Modelo 2", imei: "0099887766", memoria: 456);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
